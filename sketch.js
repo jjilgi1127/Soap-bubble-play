8de6b98983f349e16335ccde670caf4e15a7fd21
jjilgi1127 <73861696+jjilgi1127@users.noreply.github.com>
2020-11-08 21:36:08 +0900

    p5js 텍스트 추가

--- sketch.js
+++ sketch.js
@@ -22,6 +22,9 @@ function draw(){
   fill(215,95,201);
   rect(500,370,10,70);
 
+  textSize(20);
+  fill(0, 102, 153,180);
+  text('Please click on the mouse', 10, 380);
 
 
   for(var i = 0; i < bubbles.length; i++){
