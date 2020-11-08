34704df8527eec13c7ea7907f8a26209227ed9d2
jjilgi1127 <73861696+jjilgi1127@users.noreply.github.com>
2020-11-08 21:42:54 +0900

    p5js 비눗방울 불투명도 조절

--- bubbles.js
+++ bubbles.js
@@ -3,21 +3,21 @@ function Bubble(x,y,d){
   this.vel = createVector(0,0);
   this.acc = createVector(0,0);
   this.diameter = d;
-  
+
   this.applyForce = function(force){
     this.acc.add(force);
   }
-    
+
   this.update = function(){
     this.vel.add(this.acc);
     this.pos.add(this.vel);
     this.acc.set(0,0);
-    
+
   }
-    
+
   this.display = function(){
     noStroke();
-      fill(255,100);
+      fill(255,80);
       ellipse(this.pos.x, this.pos.y,d);
   }
 }
